0eb56ebeb3319b37f407e7994823f1b01ff0e677
Emylie <167892046+EmyliePm@users.noreply.github.com>
2024-05-29 14:48:10 +0100

    update index.js to get timezones for 3 avaiulable cities on dropdown and alert them when selected.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 function showSelectedCity(event) {
-  console.log(event.target.value);
+  let getTokyoTime = moment()
+    .tz("Asia/Tokyo")
+    .format("dddd, MMMM D, YYYY h:m A");
+  let getLondonTime = moment()
+    .tz("Europe/London")
+    .format("dddd, MMMM D, YYYY h:m A");
+  let getParisTime = moment()
+    .tz("Europe/Paris")
+    .format("dddd, MMMM D, YYYY h:m A");
   if (event.target.value === "tokyo") {
-    alert("Tokyo");
+    alert(`It is ${getTokyoTime} in Asia/Tokyo`);
   }
   if (event.target.value === "london") {
-    alert("London");
+    alert(`It is ${getLondonTime} in Europe/London`);
   }
   if (event.target.value === "paris") {
-    alert("Paris");
+    alert(`It is ${getParisTime} in Europe/Paris`);
   }
 }
 let citiesSelect = document.querySelector("#cities");
