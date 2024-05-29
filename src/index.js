77810cf534a43fa51a7d7d3085e17905d6fd3727
Emylie <167892046+EmyliePm@users.noreply.github.com>
2024-05-29 14:42:07 +0100

    added moment.min.js and timezone with data

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ function showSelectedCity(event) {
   if (event.target.value === "tokyo") {
     alert("Tokyo");
   }
+  if (event.target.value === "london") {
+    alert("London");
+  }
+  if (event.target.value === "paris") {
+    alert("Paris");
+  }
 }
 let citiesSelect = document.querySelector("#cities");
 citiesSelect.addEventListener("change", showSelectedCity);
