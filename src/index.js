941e59dda46022add14801ad01dc111fd504a3c3
Emylie <167892046+EmyliePm@users.noreply.github.com>
2024-05-29 14:39:07 +0100

    added js function showSelectedCity on event , moved the script link to proper place...

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-
+function showSelectedCity(event) {
+  console.log(event.target.value);
+  if (event.target.value === "tokyo") {
+    alert("Tokyo");
+  }
+}
+let citiesSelect = document.querySelector("#cities");
+citiesSelect.addEventListener("change", showSelectedCity);
